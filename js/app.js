706202907e3fd63b82c376f3efd71f873d293d24
Ranee <67798875+RaneeDEV@users.noreply.github.com>
2021-02-13 00:45:53 +0300

    Add Filtering
    Fixes:
    - add new design
    - ordered CSS & JS

--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@ let CARS = [...DATA];
 const carListEl = document.getElementById("carList");
 const masonryBtnsEl = document.getElementById("masonryBtns")
 const sortingSelectEl = document.getElementById("sortingSelect")
+
 const searchFormEl = document.getElementById("searchForm")
+const filterFormEl = document.getElementById("filterForm")
+
+const filterFields = ['make', 'fuel', 'transmission']
 
 // ================= CONVERT TIMW START =================
 const dateFormatter = new Intl.DateTimeFormat()
@@ -31,38 +35,48 @@ const currencyFormatter = new Intl.NumberFormat(undefined, {
 insertCards(carListEl, CARS);
 
 function insertCards(whereEl, cars) {
+
   let html = "";
   cars.forEach((car) => {
     html += createCardElement(car);
   });
   whereEl.innerHTML = ""; //!!!
   whereEl.insertAdjacentHTML("beforeEnd", html);
+
 }
 // ================= CAR CARD GENERATE END ================= 
 
 // ================= SORTING START ================= 
-sortingSelectEl.addEventListener('change', event =>{
+sortingSelectEl.addEventListener('change', event => {
+
   const [key, type] = event.target.value.split('-')
 
   CARS.sort((a,b) => {
+
     if (type == 'ab') {
       return a[key] - b[key] 
-    } else if (type == 'ba') {
+
+    } else if (type == 'ba'){
       return b[key] - a[key] 
+
     }
   })
+
   insertCards(carListEl, CARS);
+
 })
+
 // ================= SORTING END ================= 
 
 // ================= SEARCH START ================= 
 searchFormEl.addEventListener('submit', event => {
+  
   event.preventDefault()
-  const query = event.target.search.value.toLowerCase().trim().split(' ')
-  console.log(query);
-  const filterFields = ['make', 'model', 'year', 'engine_volume']
 
-  const filteredCars = CARS.filter(car => {
+  const query = event.target.search.value.toLowerCase().trim().split(' ')
+  const filterFields = ['make', 'model', 'transmission', 'engine_volume']
+
+  const filterCars = CARS.filter(car => {
     return query.every(word => {
       return !word || filterFields.some(field => {
         return `${car[field]}`.toLowerCase().trim().includes(word)
@@ -70,24 +84,97 @@ searchFormEl.addEventListener('submit', event => {
     })
   })
 
-
-  insertCards(carListEl, filteredCars);
   event.target.reset()
+  insertCards(carListEl, filterCars);
+
 })
+
 // ================= SEARCH END ================= 
 
+// ================= FILTER START ================= 
+renderFilterBlocks(filterFormEl, filterFields, CARS)
+filterFormEl.addEventListener('submit', function (event) {
+  
+  event.preventDefault()
+  const filterOptions = {}
+
+  filterFields.forEach( field => {
+    const checkedValues = [...this[field]].reduce((accu, input) => {
+      if (input.checked) {
+        return [...accu, input.value]
+      } else {
+        return accu
+      }
+    }, [])
+    filterOptions[field] = checkedValues
+  })    
+
+    const filterCars = CARS.filter(car => {
+      return filterFields.every(field => {
+        return !filterOptions[field].length || filterOptions[field].some(value => {
+          return `${car[field]}`.includes(value)
+        })
+      })
+    })
+
+    insertCards(carListEl, filterCars);
+
+})
+
+function renderFilterBlocks(whereEl, fields, cars) {
+ 
+  let html = "";
+  fields.forEach( field => {
+    html += createFilterBlock(cars, field);
+  })
+  whereEl.insertAdjacentHTML("afterbegin", html);
+}
+
+function createFilterBlock(cars, field) {
+ 
+  let inputs = ''
+
+  const uValues = [...new Set(cars.map(car => car[field]).sort())]
+
+  uValues.forEach((value) => {
+    inputs += createFilterElement(field, value)
+  });
+
+  return `<fieldset class="filter-block border-top mb-3">
+  <legend class="filter-fields fw-bold text-uppercase fs-4">${field}</legend>
+  <div class="d-flex flex-column filter-elements fs-5">
+  ${inputs}
+  </div>
+  </fieldset>`
+
+}
+
+function createFilterElement(field, value) {
+  return `<label>
+  <input type="checkbox" name="${field}" value="${value}">
+  ${value}
+  </label>`
+}
+
+// ================= FILTER END ================= 
+
 // ================= BTN LIST CHANGEs START ================= 
+
 masonryBtnsEl.addEventListener('click', function (event) {
+
   const btnEl = event.target.closest('.button')
+  
   if (btnEl) {  
     const btnAction = btnEl.dataset.action
     if (btnAction == '1') {
       carListEl.classList.remove('row-cols-2')
       carListEl.classList.add('row-cols-1')
+
     } else if (btnAction == '2') {
       carListEl.classList.remove('row-cols-1')
       carListEl.classList.add('row-cols-2')
     }
+
     const currentBtnSiblings = Array.from(this.children).filter(element => element != btnEl)
     btnEl.classList.remove('btn-secondary')
     btnEl.classList.add('btn-success')
@@ -96,13 +183,16 @@ masonryBtnsEl.addEventListener('click', function (event) {
       element.classList.add('btn-secondary') 
     })
   }
+
 })
+
 // ================= BTN LIST CHANGEs END ================= 
 
 
 function createCardElement(car) {
 
   // ======== STARTS RATING START ======== 
+
   let starsHtml = ''
 
   for (let i = 0; i < 5; i++) {
@@ -114,9 +204,10 @@ function createCardElement(car) {
     starsHtml += `<i class="far fa-star"></i>`
     }
   }
+
   // ======== STARTS RATING END ======== 
 
-  return `<div class="col card mb-2 ">
+  return `<div class="col card mb-2">
     <div class="row g-0">
       <div class="col-4 position-relative card-img-wrap mt-3">
         <img width="1" height="1" loading="lazy" class="card-img" src="${car.img}" alt="${car.make} ${car.model} ${car.year}">
@@ -126,7 +217,7 @@ function createCardElement(car) {
       </div>
       <div class="col-8 card-body-wrap">
         <div class="card-body">
-          <h2 class="col-9 card-title text-primary fs-4">${car.make} ${car.model} ${car.engine_volume} (${car.year})</h2>
+          <h2 class="col-9 card-title text-primary fw-bold fs-4">${car.make} ${car.model} ${car.engine_volume} (${car.year})</h2>
           <div class="col-3 d-flex align-items-center justify-content-end">
         </div>
 
@@ -150,7 +241,7 @@ function createCardElement(car) {
           <p class="col card-text card-fuel engine-volume"><i class="fas fa-sync"></i>${car.consume?.mixed}</p>
         </div>
 
-          ${car.vin ? `<p class="card-icons card-vin border border-2 rounded-end border-primary"><i class="fas fa-car-alt text-light p-2 bg-primary"></i>${car.vin}</p>` : `<p class="card-icons card-vin border border-2 rounded-end border-warning text-uppercase"><i class="fas fa-exclamation-triangle p-2 bg-warning"></i>This car has not VIN number!</p>`}
+          ${car.vin ? `<p class="card-icons card-vin border border-2 rounded-end border-primary"><i class="fas fa-car-alt text-light p-2 bg-primary"></i>${car.vin}</p>` : `<p class="card-icons card-vin border border-2 rounded-end border-warning fw-bold text-uppercase"><i class="fas fa-exclamation-triangle p-2 bg-warning"></i>Autor didn't write a VIN</p>`}
           
         <div class="card-icons">
           <p class="car-inf"><i class="fas fa-fill-drip"></i>${car.color}</p>
